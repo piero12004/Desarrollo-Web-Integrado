a722e51f58526a7f98a1428020901443ce6678c4
piero12004 <pierobajonero.12004@gmail.com>
2025-11-20 19:24:33 -0500

    estructura backend

---
+++ Backend/compre/src/main/java/com/utp/compre/CompreApplication.java
@@ -0,0 +1,13 @@
+package com.utp.compre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompreApplication.class, args);
+	}
+
+}
---
+++ Backend/compre/src/test/java/com/utp/compre/CompreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utp.compre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
