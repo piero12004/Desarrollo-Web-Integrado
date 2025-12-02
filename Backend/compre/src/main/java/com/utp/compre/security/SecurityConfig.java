4f9749befa219fc226dbf6d1fbc5dac051c1da39
piero12004 <pierobajonero.12004@gmail.com>
2025-12-01 21:38:42 -0500

    cors

--- Backend/compre/src/main/java/com/utp/compre/security/SecurityConfig.java
+++ Backend/compre/src/main/java/com/utp/compre/security/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/usuario/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
